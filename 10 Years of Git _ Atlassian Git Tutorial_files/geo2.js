12801f961c48c1766670753c699fac507d080fbb
jishuiyuren <ruby081214@163.com>
2018-09-07 16:32:06 +0800

    first commit

---
+++ 10 Years of Git _ Atlassian Git Tutorial_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "SHANGHAI",
+      'continent': "AS",
+      'country': "CN",
+      'region': "SH"
+    },
+    'ip':"61.171.209.89"
+  }]);
+})
+//
+()
+
+;
---
+++ detail.php
@@ -0,0 +1,8 @@
+<div id="cc">
+    <ul>
+        <?php for($i=0;$i<6;$i++):?>
+        <li></li>
+        <?php endfor;?>
+    </ul>
+</div>
+
